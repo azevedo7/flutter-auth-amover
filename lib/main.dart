c3a3cd04fc023da4c7df19d161923533472fcc8e
azevedo <jazevedo0406@gmail.com>
2025-01-07 11:45:22 +0000

    Using dio for direct access grants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
 import 'package:flutter_appauth/flutter_appauth.dart';
 
 FlutterAppAuth appAuth = FlutterAppAuth();
@@ -43,29 +45,58 @@ class _LoginState extends State<Login> {
 
     const String clientId = 'flutter-client';
     const String redirectUrl = 'com.example.app:/oauthredirect';
-    const String issuer = 'http://localhost:8080/realms/appAuth';
+    const String issuer = 'http://localhost:8843/realms/appAuth';
+    const String tokenUrl =
+        'http://localhost:8843/realms/appAuth/protocol/openid-connect/token';
 
     try {
-    final AuthorizationTokenResponse? result = await appAuth.authorizeAndExchangeCode(
-      AuthorizationTokenRequest(
-        clientId,
-        redirectUrl,
-        issuer: issuer,
-        scopes: ['openid', 'profile', 'email'],
-      ),
-    );
+      // final AuthorizationTokenResponse? result = await appAuth.authorizeAndExchangeCode(
+      //   AuthorizationTokenRequest(
+      //     clientId,
+      //     redirectUrl,
+      //     issuer: issuer,
+      //     scopes: ['openid', 'profile', 'email'],
+      //   ),
+      // );
 
-    if (result != null) {
-      print('Access Token: ${result.accessToken}');
-      print('ID Token: ${result.idToken}');
-      // Navigate to a logged-in page or perform authenticated actions
+      // if (result != null) {
+      //   print('Access Token: ${result.accessToken}');
+      //   print('ID Token: ${result.idToken}');
+      //   // Navigate to a logged-in page or perform authenticated actions
+      // }
+
+      Dio dio = Dio();
+
+      final response = await dio.post(tokenUrl,
+              data: {
+                'grant_type': 'password',
+                'client_id': clientId,
+                'username': email,
+                'password': password,
+              },
+              options: Options(contentType: Headers.formUrlEncodedContentType),
+            );
+
+      if (response.statusCode == 200) {
+        final accessToken = response.data['access_token'];
+        final refreshToken = response.data['refresh_token'];
+
+        print('Access Token: $accessToken');
+        print('Refresh Token: $refreshToken');
+        // Navigate to a logged-in page or store tokens securely
+      } else {
+        print('Login failed: ${response.data}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Invalid credentials')),
+      );
     }
-  } catch (e) {
-    print('Error during login: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Login failed')),
-    );
-  }
+    } catch (e) {
+      print('Error during login: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed')),
+      );
+    }
+    
   }
 
   @override
@@ -83,31 +114,32 @@ class _LoginState extends State<Login> {
               Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: 
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your email";
-                    }
-                    return null;
-                  },
-                ),
-              
+              child: TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your email";
+                  }
+                  return null;
+                },
+              ),
             ),
-            TextFormField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Please enter your email";
-                }
-                return null;
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your password";
+                  }
+                  return null;
+                },
+              ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
